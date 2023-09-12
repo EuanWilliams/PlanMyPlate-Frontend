bdf5c7f7e10733b724021b37200cca13314452bf
Euan Williams <euan.williams01061999@gmail.com>
2023-09-12 22:16:17 +0100

    Kinda got recipes list working with api but it doesnt seem like great practice. Added components for recipe_list and recipe_list item

--- src/pages/recipes/recipe_list_item.js
+++ src/pages/recipes/recipe_list_item.js
@@ -1,42 +1,16 @@
 import React from 'react';
 
 import './recipes.css';
-import Header from '../../components/header/header';
 
 
-class Recipes extends React.PureComponent {
-  constructor(props) {    
-    super(props);
-    this.state = {
-      recipes: this.getRecipes()
-    };
-  }
-
+class RecipeListItem extends React.PureComponent {
   render() {
     return (
-      <div className="Recipes">
-        <Header />
-        <div className='recipes_container'>
-        </div>
+      <div className="recipe_list_item">
+        <h1 className="recipe_title">{ this.props.recipe_name }</h1>
       </div>
     );
   }
-
-  // get recipes.
-  getRecipes() {
-    // Get user id from jwt.
-    var user_id = JWTUtils.get_user_id();
-  
-    // Get recipes from api.
-    // TODO: Get url from environment.
-    let recipes_url =  "http://127.0.0.1:5000/recipes/" + user_id; 
-  
-    console.log("Requesting recipes from: " + recipes_url);
-
-    fetch(recipes_url)
-      .then(response => response.json())
-      .then(data => console.log(data));
-  }
 }
 
-export default Recipes;
+export default RecipeListItem;
--- src/pages/recipes/recipes.js
+++ src/pages/recipes/recipes.js
@@ -2,41 +2,18 @@ import React from 'react';
 
 import './recipes.css';
 import Header from '../../components/header/header';
-import JWTUtils from '../../utils/jwt_utils';
+import RecipeList from './recipe_list';
 
 
 class Recipes extends React.PureComponent {
-  constructor(props) {    
-    super(props);
-    this.state = {
-      recipes: this.getRecipes()
-    };
-  }
 
   render() {
     return (
       <div className="Recipes">
         <Header />
-        <div className='recipes_container'>
-        </div>
+        <RecipeList className="recipes_list" />
       </div>
     );
-  }
-
-  // get recipes.
-  getRecipes() {
-    // Get user id from jwt.
-    var user_id = JWTUtils.get_user_id();
-  
-    // Get recipes from api.
-    // TODO: Get url from environment.
-    let recipes_url =  "http://127.0.0.1:5000/recipes/" + user_id; 
-  
-    console.log("Requesting recipes from: " + recipes_url);
-
-    fetch(recipes_url)
-      .then(response => response.json())
-      .then(data => console.log(data));
   }
 }
 
